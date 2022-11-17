10a7d059cd655777fdcc92359f43fb9e500d41d1
Mohammod Al Amin Ashik <mash@sitecore.net>
2022-11-18 01:57:34 +0800

    added services listing

---
+++ src/AppCore/Services/K8s/ServicesService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace AppCore.Services.K8s;
+
+public class ServicesService
+{
+    private readonly CurrentK8SContext _context;
+
+    public ServicesService(CurrentK8SContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IList<V1Service>> GetAllAsync()
+    {
+        return (await _context.Client.Client!.CoreV1.ListNamespacedServiceAsync("")).Items;
+    }
+    public async Task<IList<V1Service>> GetNamespaced()
+    {
+        if (!_context.ActiveNamespaceList.IsNullOrEmpty())
+        {
+            var services = new List<V1Service>();
+            foreach (var v1Namespace in _context.ActiveNamespaceList)
+            {
+                var nServices = (await _context.Client.Client!.CoreV1.ListNamespacedServiceAsync(v1Namespace.Name())).Items;
+                services.AddRange(nServices);
+            }
+            return services;
+
+        }
+        else
+        {
+            return await GetAllAsync();
+        }
+
+    }
+
+}
---
+++ src/BlazorMauiAppClient/ViewModels/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using k8s.Models;
+
+namespace BlazorMauiAppClient.ViewModels;
+
+public abstract class BaseViewModel
+{
+    public abstract string Name { get; set; }
+    public abstract string Namespace { get; set; }
+    public abstract string Age { get; set; }
+
+    public string GetAgeString(DateTime creationDateTime)
+    {
+        var diff = DateTime.Now - creationDateTime;
+        string ageStr = string.Empty;
+        if (diff.Days > 0)
+        {
+            ageStr += diff.Days + "d ";
+        }
+
+        if (diff.Hours > 0)
+        {
+            ageStr += diff.Hours + "h ";
+        }
+        if (diff.Minutes > 0)
+        {
+            ageStr += diff.Minutes + "m ";
+        }
+        return ageStr;
+    }
+}
